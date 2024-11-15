38386f1ff03ede89c02207d3a46ace1e926592ff
Dennis <dennisadev@gmail.com>
2024-11-15 09:31:15 -0500

    initial commit

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,28 @@
 {
     public class Program
     {
+        public static void StoryCreation()
+        {
+            //collect user input for story
+            Console.WriteLine("What is your name?");
+            string username = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string favcolor = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal");
+            string favanimal = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string favband = Console.ReadLine();
+
+            //write story
+            Console.WriteLine($"{username}'s favorite color is {favcolor}, favorite animal is a {favanimal}, and favorite band are the {favband}");
+        }
+        
+        
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //call story creation method
+            StoryCreation();
         }
     }
 }
