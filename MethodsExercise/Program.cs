8cf20308b5b98d0f157795ae2dc4247999859758
Dennis <dennisadev@gmail.com>
2024-11-15 10:32:34 -0500

    commit with math operatiors

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace MethodsExercise
+﻿using System;
+using System.Data;
+using System.Globalization;
+
+namespace MethodsExercise
 {
     public class Program
     {
@@ -18,12 +22,46 @@
             Console.WriteLine($"{username}'s favorite color is {favcolor}, favorite animal is a {favanimal}, and favorite band are the {favband}");
         }
         
-        
-        
+        //Methods for Math Operations
+        public static int Add(int a, int b)
+         {
+             return a + b; 
+         }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        public static int Sum(params int[] numbers) 
+         { 
+            int sum = 0;
+            foreach (int number in numbers) 
+              {
+                sum += number; 
+              }
+            return sum;  
+         }
+
         static void Main(string[] args)
         {
             //call story creation method
             StoryCreation();
+
+            //call and write Math operation methods
+            Console.WriteLine(Add(a: 2, b: 2));
+            Console.WriteLine(Subtract(a: 2, b: 2));
+            Console.WriteLine(Multiply(a: 2, b: 2));
+            Console.WriteLine(Divide(a: 2, b: 2));
+            Console.WriteLine(Sum(2,2,2));
         }
     }
 }
